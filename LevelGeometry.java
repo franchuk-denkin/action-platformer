7910429196bb58d7b85ad7bab98c52fad99af69f
Roman Franchuk <franchukrom@gmail.com>
2013-04-21 13:47:05 +0300

    Тепер паралельне перенесення при відображенні рівнів не впливає на решту об’єктів
    AC-21 #close

--- LevelGeometry.java
+++ LevelGeometry.java
@@ -15,6 +15,7 @@ public class LevelGeometry implements GeometricObject, Drawable {
     public void draw(Graphics2D canvas) {
         canvas.translate(engine.width() / 2, engine.height() / 2);
         canvas.drawImage(visual, -visual.getWidth() / 2, -visual.getHeight() / 2, null);
+        canvas.translate(-engine.width() / 2, -engine.height() / 2);
     }
 
     @Override
